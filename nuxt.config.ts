279ff3f2ae908a2d69ee2d4b3d41ade48d3a7462
Thanchanok Tipmaneekomut <thanchatip.coe@gmail.com>
2024-11-11 14:56:57 +0700

    uxuy integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ const MyPreset = definePreset(Lara, {
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  ssr: false, // to fix cannot read addEventListener because of it's client side command
   modules: ["@pinia/nuxt", "@primevue/nuxt-module", "@nuxtjs/tailwindcss"],
   primevue: {
     usePrimeVue: true,
