816740a0ee3f27b8a1250b03f682f54b8d4f8c5b
Thanchanok Tipmaneekomut <thanchatip.coe@gmail.com>
2024-11-11 16:45:26 +0700

    connect telegram user information

--- pages/connect-wallet.vue
+++ pages/connect-wallet.vue
@@ -6,6 +6,7 @@
         icon="pi pi-wallet"
         @click="handleClickConnectWallet"
       />
+      <div class="mt-4">telegram param : {{ user }}</div>
     </div>
   </div>
 </template>
@@ -14,18 +15,35 @@
 const { $uxuy } = useNuxtApp();
 
 const user = ref(null);
+const telegramInfo = ref();
 
 onMounted(() => {
-  // Ensure Telegram is available and get user data
+  // Ensure Telegram WebApp object is available
   if (typeof window !== "undefined" && window.Telegram) {
-    window.Telegram.WebApp.ready();
-    user.value = window.Telegram.WebApp.initDataUnsafe?.user;
+    // Initialize Telegram Web App
+    const tg = window.Telegram.WebApp;
 
-    console.log("User info:", user.value); // Display user info in console for debugging
+    tg.ready(); // Tells Telegram that the app is ready
+
+    // Expand the Web App to full height
+    tg.expand();
+    const theme = tg.themeParams;
+    console.log("Current Telegram theme:", theme);
+
+    // Retrieve and store user info
+    user.value = tg.initDataUnsafe?.user;
+    console.log("Telegram user info:", user.value);
   } else {
     console.error("Telegram Web App API is not available.");
   }
 });
+
+// Define a method to expand the Web App when a button is clicked
+function expandApp() {
+  if (window.Telegram) {
+    window.Telegram.WebApp.expand();
+  }
+}
 
 async function handleClickConnectWallet() {
   const response = await $uxuy.initializeWallet();
