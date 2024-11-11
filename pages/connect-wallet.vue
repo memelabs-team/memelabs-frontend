e01184816bcb031800394ee74ce6270bbec687e1
Thanchanok Tipmaneekomut <thanchatip.coe@gmail.com>
2024-11-11 16:05:14 +0700

    update telegram integration

--- pages/connect-wallet.vue
+++ pages/connect-wallet.vue
@@ -13,6 +13,22 @@
 <script setup>
 const { $uxuy } = useNuxtApp();
 
+const user = ref(null);
+
+onMounted(() => {
+  Telegram.WebApp.ready();
+
+  user.value = Telegram.WebApp.initDataUnsafeM.user;
+
+  console.log("telegram user:", user.value);
+
+  if (user.value) {
+    console.log("User is already logged in: ", user);
+  } else {
+    console.log("User is not logged in yet.");
+  }
+});
+
 async function handleClickConnectWallet() {
   const response = await $uxuy.initializeWallet();
   const uxuyAccount = response.userAddress;
